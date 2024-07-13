cf3e2c333df47f20868c660600748de003ebe16a
Ujitha <ujithamigara@gmail.com>
2024-07-13 14:18:14 +0530

    implement RedTech

--- main.py
+++ main.py
@@ -90,8 +90,59 @@ class NanoTek(scrapy.Spider):
 
         yield loader.load_item()
 
+class RedTech(scrapy.Spider):
 
+    name = "redtech"
 
+    def start_requests(self):
+        urls = [
+            "https://redtech.lk/product-category/laptops-notebooks/",
+        ]
+        for url in urls:
+            yield Request(url=url, callback=self.parse_items)
+
+    def parse_items(self, response):
+
+        for product in response.css("li.product"):
+            loader = ItemLoader(item=ProductItem(), selector=product)
+            loader.add_css("title", "h2.woocommerce-loop-product__title")
+            loader.add_css("price", "span.woocommerce-Price-amount bdi::text")
+            loader.add_value("url", product.css("a.woocommerce-LoopProduct-link::attr(href)").get())
+            loader.add_css("image", "img.attachment-woocommerce_thumbnail::attr(src)")
+            loader.add_css("description", "div.product-short-description")
+
+            item = loader.load_item()  # Load the item
+            raw_title = item.get("title")  # Access the 'title' field from the loaded item
+            self.logger.info(f"Raw Title: {raw_title}")
+
+            cleaned_title, description = clean_title_and_description_alternative(raw_title)
+            loader.replace_value("title", cleaned_title)
+            loader.replace_value("description", description)
+
+            item = loader.load_item()
+
+            yield item
+
+        next_page = response.css('a.page-numbers::attr(href)').get()
+        if next_page:
+            self.logger.info(f"Following next page: {next_page}")
+            yield response.follow(next_page, self.parse_items)
+        else:
+            self.logger.info("No next page found.")
+    pass
+def clean_title_and_description_alternative(raw_title):
+    if raw_title is None:
+        return None, None
+
+    phrase = "– Order Now! – "
+
+    raw_title = raw_title.replace(phrase, "")
+
+    # Example cleaning process
+    cleaned_title = raw_title.strip()
+    description = cleaned_title
+
+    return cleaned_title,description
 process = CrawlerProcess(
     settings={
         "FEEDS": {
@@ -105,8 +156,8 @@ process = CrawlerProcess(
     }
 )
 
-process.crawl(LaptopLK)
+# process.crawl(LaptopLK)
 # process.crawl(RedlineTech)
-# process.crawl(ChamaComputers)
-process.crawl(NanoTek)
+process.crawl(RedTech)
+# process.crawl(NanoTek)
 process.start()
